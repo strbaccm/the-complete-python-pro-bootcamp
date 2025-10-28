2458e1e2b56b9e4bee0ab7647fbc3f6010e56f8b
Milica Štrbac <110546728+strbaccm@users.noreply.github.com>
2025-10-28 15:19:47 +0100

    Add files via upload

---
+++ pong-game/paddle.py
@@ -0,0 +1,25 @@
+import turtle
+from turtle import Turtle
+
+
+
+class Paddle(Turtle):
+
+    def __init__(self,position):
+        super().__init__()
+        self.create_paddle(position)
+
+    def create_paddle(self, position):
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid= 5, stretch_len= 1)
+        self.goto(position)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
